dcadceb965c8b596a7957e623da551b32b72374e
howwant <corn1128@naver.com>
2025-05-25 23:08:30 +0900

    test3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/IntAmericanoriorForm/",
+  base: "/Americano",
   build: {
     rollupOptions: {
       output: {
